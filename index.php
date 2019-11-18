31e3f3736919018ea8a1d1c2586a12b71c35dbed
Igor Pereira <igorfortalezapereira@gmail.com>
2019-11-18 13:49:19 -0300

    criacao_do_escopo_do_projeto

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset = "UTF-8">
+    <title>PROJETO</title>
+    <link rel="stylesheet" type="text/css" hfef="css/style.css">
+</head>
+<body>
+    <?php
+        echo "Hello Word";
+    ?>
+</body>
+</html>
+
